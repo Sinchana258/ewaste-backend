b9f1aebcd8b8e8f6752a3d9b56f437c8faa70790
sinchana258 <sinchanat826@gmail.com>
2026-02-08 14:37:22 +0530

    Fix CORS with vercel regex

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https://.*\.vercel\.app",
+    allow_origin="https://ewaste-frontend-bice.vercel.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
